e40fd7dc6883b78bb0fdf9171ff54eb73738b4c5
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-07 09:22:10 -0600

    Create Student.java

---
+++ src/edu/mu/Student.java
@@ -0,0 +1,12 @@
+package edu.mu;
+
+public class Student {
+	public static void main(String[] args) {
+		Student[] objectArrayReference = new Student[5];
+
+	}
+	//toString
+	//setting up student class
+	//equals
+	//print out student manager information
+}
