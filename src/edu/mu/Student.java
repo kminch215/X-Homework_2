721062628dc543d0386ec7b4fea02e614d93e333
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-07 09:31:53 -0600

    Update Student.java

--- src/edu/mu/Student.java
+++ src/edu/mu/Student.java
@@ -1,6 +1,10 @@
 package edu.mu;
 
 public class Student {
+	private int Student = 4;//id;
+	private String name = "name"; //import from studentData.txt
+	private double grade; //import from studentData.txt
+	
 	public static void main(String[] args) {
 		Student[] objectArrayReference = new Student[5];
 
