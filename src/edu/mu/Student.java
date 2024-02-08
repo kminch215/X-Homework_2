5cafe3f3d5925ac07c3f500025b84db9d4abf077
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-08 12:38:19 -0600

    Update Student.java

--- src/edu/mu/Student.java
+++ src/edu/mu/Student.java
@@ -1,8 +1,33 @@
 package edu.mu;
 
 public class Student {
-	private int id; //import from studentData.txt
-	private String name; //import from studentData.txt
+	private int Student = 4;//id;
+	/**
+	 * 
+	 */
+	private Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	private int getStudent() {
+		return Student;
+	}
+	private void setStudent(int student) {
+		Student = student;
+	}
+	private String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	private double getGrade() {
+		return grade;
+	}
+	private void setGrade(double grade) {
+		this.grade = grade;
+	}
+	private String name = "name"; //import from studentData.txt
 	private double grade; //import from studentData.txt
 	
 	//toString
