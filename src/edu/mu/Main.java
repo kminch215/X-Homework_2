4a7f773c77a2229d90b1a277ee2f183214f70347
kminch215 <kendraminch1036@gmail.com>
2024-02-09 18:14:16 -0600

    Changes made to the Main file to test functionality

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -4,10 +4,18 @@ package edu.mu;
 public class Main {
 
 	public static void main(String[] args) {
-		StudentManager m1 = new StudentManager();
-		m1.readFromFile("file/studentData.txt");
-		m1.displayStudents();
-
+		// Instantiate StudentManager, perform operations based on the requirements.
+		StudentManager studentManager = new StudentManager();
+		// Read student data from a file and initialize Student objects.
+		boolean fileReadStatus = studentManager.readFromFile("studentData.txt");
+		// Display all students.
+		studentManager.displayStudents();
+		// Search for a student by ID.
+		boolean studentFound = studentManager.searchStudentById(101);
+		// Update the grade of a student by ID.
+		boolean studentGradeUpdateStatus = studentManager.updateStudentGradeById(102, 95);
+		// Display all students after the update.
+		studentManager.displayStudents();
 	}
 
 }
