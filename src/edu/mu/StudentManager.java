64e3a86f42ed360eb2cba51153e39266ed72bca7
kminch215 <kendraminch1036@gmail.com>
2024-02-09 18:19:32 -0600

    Removing testing processes and adding comments

--- src/edu/mu/StudentManager.java
+++ src/edu/mu/StudentManager.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 
+//This is the StudentManager class that contains the main logic for this assignment
 public class StudentManager {	
 	
+	//creating the instance variable which is an array of Student objects
 	private Student[] students;
 	
+	//default constructor
 	public StudentManager() {
 		this.students = new Student[5];
 	};
 	
-	public Student[] getStudents() {
-		return students;
-	}
-	
-	public void setStudents(Student[] students) {
-		this.students = students;
-	}
-		
+	//This method is used to display all of the students in the Student object array
+	//This method will take no parameters and won't return anything to the calling class/method
 	public void displayStudents() {
 		for(int i=0; i<students.length; i++) {
 			if(students[i] != null) {
@@ -32,22 +29,30 @@ public class StudentManager {
 
 	}
 	
+	//This method is used to search the Student object array for a specific student based on ID
+	//This method will take in an integer representing the id and return a boolean value to indicate if it was found
 	public boolean searchStudentById(int id) {
 		for(int i = 0; i < students.length; i++)
 		{
+			//uses the equals method in the Student class
 			boolean found = students[i].equals(id);
 			if(found)
 			{
-				System.out.println(students[i]);
+				//will return true if the student is found
 				return true;
 			}
 			
 		}
-		
-		return false; //if student ID was not found
+		//returns false if the student is not found
+		return false; 
 	}
 
+	
+	//This method will update a students grade. It will find the correct student by using their ID
+	//This method will take in an integer ID value and a double grade value and return a boolean value to 
+	//indicate if the students grade has been updated
 	public boolean updateStudentGradeById(int id, double grade) {
+		//will use the searchStudentById to determine if the student ID is in the Student object array
 		if(this.searchStudentById(id))
 		{
 			for(int i = 0; i < students.length; i++)
@@ -56,17 +61,18 @@ public class StudentManager {
 				if(found)
 				{
 					students[i].setGrade(grade);
-					//delete before submission
-					System.out.println(students[i].toString());
-
+					
+					//will return true if the update was successful
 					return true;
 				}
 			}
 		}
-		
-		return false; //if not found
+		//will return false if the update was not successful or the student ID wasn't able to be matched
+		return false;
 	}
 
+	//This method will read from a file and store the contents of that file into a Student object array
+	//This method will take in a string that is the fileName and then return a boolean value if the file was read correctly
 	public boolean readFromFile(String fileName) {
 		try {
 			int i=0;
@@ -82,8 +88,10 @@ public class StudentManager {
             fileIn.close();
         } catch(FileNotFoundException e){
         	e.printStackTrace();
+        	//return false if the file was correctly read
             return false;
         }
+		//return true if the file was correctly read
 		return true;
 	}
 	
